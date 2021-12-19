d0e9e357dfaeff576a72bb1c32daf60363411d2d
Kousik K A <kakousigowtham@gmail.com>
2021-12-20 00:13:49 +0530

    platformChannelMessages_API-2

--- src/components/MessagesByPlatformChannels/MessagesByPlatformChannels.jsx
+++ src/components/MessagesByPlatformChannels/MessagesByPlatformChannels.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 const MessagesByPlatformChannels = ({
   platformChannelMessages,
   selectedPlatformChannel,
 }) => {
-  const navigate = useNavigate();
-
   console.log(platformChannelMessages, selectedPlatformChannel);
   return (
     <>
