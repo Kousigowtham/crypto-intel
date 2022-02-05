7710ac26be95867f4ea9f1f59dd0872355a297f0
Kousik K A <kakousigowtham@gmail.com>
2022-02-05 23:37:04 +0530

    Reverting baseUrl to herokuapp

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "//ec2-35-175-145-167.compute-1.amazonaws.com:8080/institute",
+  // baseURL: "//ec2-35-175-145-167.compute-1.amazonaws.com:8080/institute",
+  baseURL: "https://cryptointelligence.herokuapp.com/institute",
 });
